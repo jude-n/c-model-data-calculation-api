514444653fdd1219e18067b0eae397976ed6a8a1
del <delcoker@gmail.com>
2022-07-06 20:50:25 +0200

    add quarter and product_id and product_name

--- cmodel/view_models/test/create_booking_from_df.py
+++ cmodel/view_models/test/create_booking_from_df.py
@@ -1,21 +1,23 @@
 import json
 import pandas as pd
 
-from django.core import serializers
-
 from cmodel.view_models.booking_class import Booking
 from cmodel.view_models.model_data_class import ModelData
 from cmodel.view_models.product_data_class import ProductData
 from cmodel.view_models.time_period_class import TimePeriod
 
-
 df_joined_data = pd.read_pickle('join_data_df.pkl')
 
 bookings = []
 
+quarter_needs_to_be_removed = 0
 for i, row in df_joined_data.iterrows():
-    timePeriod = TimePeriod(month=row['month'], monthName=row['monthName'], year=row['year']).__dict__
-    productData = ProductData(name=row['Group Name'], totalRevenue=row['c-model']).__dict__
+
+    if i % 3 == 0:
+        quarter_needs_to_be_removed = quarter_needs_to_be_removed + 1
+
+    timePeriod = TimePeriod(quarter=quarter_needs_to_be_removed, month = row['month'], monthName = row['monthName'], year = row['year']).__dict__
+    productData = ProductData(groupId=i, groupName=row['Group Name'], totalRevenue=row['c-model']).__dict__
     modelData = ModelData().__dict__
     booking = Booking(timePeriod=timePeriod, productData=productData, modelData=modelData).__dict__
     bookings.append(booking)
